4c711e4ab49f9c5e328d50f47b9ecfe9aa8c30cf
justend29 <justend29@gmail.com>
2018-12-08 23:01:52 -0400

    reformatted the output for better  legibility

--- coreController.sh
+++ coreController.sh
@@ -1,22 +1,23 @@
  #!/bin/bash
 
 # writes to the online file of each core to turn them on or off
-# November 13, 2018
+# Dec 8, 2018
 
 # command should be coreController numCores
 
 #
 #https://stackoverflow.com/questions/6481005/how-to-obtain-the-number-of-cpus-cores-in-linux-from-the-command-line
 
-available_cores=$((`nproc --all`-1))
-desired_cores=$(($1-1))
+available_cores=`nproc --all`
+desired_cores=$1
 
-echo available cores $available_cores
-echo desired cores $desired_cores
+echo Available cores: $available_cores
+echo -e Desired cores: $desired_cores
 
+echo core 1 status: 1 # print out a 1 for the required first core
 #loop through cores 1 through max. Skip core 0 as it's write protected and required
-for core in `seq 1 $available_cores`; do
-	echo current iteration $core
+for core in `seq 1 $(($available_cores-1))`; do
+	printf "%s%d%s" "core " $core " status: "
 	if [ $core -lt $1 ]
 	then
 		echo 1 | sudo tee /sys/devices/system/cpu/cpu$core/online
@@ -25,4 +26,6 @@ for core in `seq 1 $available_cores`; do
 	fi
 done
 
-echo available cores `nproc`
+echo Checking available cores...
+echo Available cores: `nproc`
+echo Done.
